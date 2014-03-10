350e2ed6cfadd4f3b4d6dd40d6bc5c4b6726fc40
JoergFiedler <joerg.fiedler@moumantai.de>
2014-03-10 23:59:15 +0100

    reverted last commit

--- home/.profile.d/python.sh
+++
@@ -1,9 +0,0 @@
-# pip should only run if there is a virtualenv currently activated
-export PIP_REQUIRE_VIRTUALENV=true
-# cache pip-installed packages to avoid re-downloading
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-
-syspip(){
-   PIP_REQUIRE_VIRTUALENV="" pip "$@"
-}
-
