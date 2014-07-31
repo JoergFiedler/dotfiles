8f70ff12e4c3ee2ac85f2e12313c3d720ff651c2
Joerg Fiedler <joerg.fiedler@immobilienscout24.de>
2014-07-31 23:13:25 +0200

    remove old ssh util stuff; enable check pub key connection

--- home/.profile.d/utils.sh
+++ home/.profile.d/utils.sh
@@ -1,42 +1,23 @@
-ssh() {
-  host=$(echo "$*" | sed 's/.*[[:space:]]\([^\S]*\)$/\1/')
-  if [ -n "${TMUX}" ]; then
-    tmux rename-window ${host}
-    command ssh "$@"
-    tmux set-window-option automatic-rename "on" 1>/dev/null
-  else
-    command ssh "$@"
-  fi
+zip_dotfiles() {
+  tar czf ${HOME}/.dotfiles_ssh.tar.gz .bash_profile .vimrc .vim .profile.d
 }
 
-copy_ssh_id_host() {
-  cat .ssh/id_rsa.pub \
-  | ssh $1 \
-     "echo copying ssh public key to $1; \
-     if test ! -d .ssh/; then \
-       mkdir .ssh; \
-       chmod 700 .ssh; \
-     fi; \
-     cat -> .ssh/authorized_keys; \
-     chmod 600 .ssh/authorized_keys"
+public_key_auth_ok() {
+  echo `command /usr/bin/ssh -o BatchMode=yes $1 echo 'ok' 2> /dev/null`
+}
+
+ssh() {
+  host=$(echo "$*" | sed 's/.*[[:space:]]\([^\S]*\)$/\1/')
+  if [ -z "$(public_key_auth_ok $host)" ]; then
+    echo 'Need to copy public key to enable pub-key authentication'
+    copy_ssh_id_host $host
+  fi
+  command ssh "$@"
 }
 
 copy_ssh_id() {
   for host in $(cat $1); do
     copy_ssh_id_host ${host}
-  done
-}
-
-copy_bash_profile_host() {
-  echo -n "$1"
-  ssh $1 "rm .bash_profile &>/dev/null"
-  ssh $1 "rm .bashrc &>/dev/null"
-  scp ~/.bash_profile_remote $1:.bash_profile &>/dev/null
-}
-
-copy_bash_profile() {
-  for host in $(cat $1); do
-    copy_bash_profile_host ${host}
   done
 }
 
