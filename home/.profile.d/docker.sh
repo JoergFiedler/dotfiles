f7aed68fe768fee7d2577ac76c1b879e063ac29f
Joerg Fiedler <joerg.fiedler@moumantai.de>
2016-04-23 09:24:09 +0200

    add safe checks to docker commands

--- home/.profile.d/docker.sh
+++ home/.profile.d/docker.sh
@@ -1,3 +1,3 @@
-source /usr/local/etc/bash_completion.d/docker
-eval "$(boot2docker shellinit)"
+[ -f /usr/local/etc/bash_completion.d/docker ] && . /usr/local/etc/bash_completion.d/docker
+[ $(which boot2docker) ] && eval "$(boot2docker shellinit)"
 
